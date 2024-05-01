5412a759cb0f0e93189c19e264ab170e68ec75d2
Javier <javier.garcia.d@gmail.com>
2024-05-01 14:29:14 -0500

    Update financials and shares_outstanding notebooks

--- edgar-filings-examples/sql/filings_by_ticker.sql
+++ edgar-filings-examples/sql/filings_by_ticker.sql
@@ -3,7 +3,7 @@
 SELECT DISTINCT r.source_report_identifier as accession_number,
     r.properties ['document_type'] as document_type,
     r.reporting_period_end_date as period_date
-FROM reports r
-    JOIN entities e ON e.entity_id = r.entity_id
-WHERE e.entity_ticker in (:tickers)
+FROM EdgarFilingsStaging.reports r
+    JOIN EdgarFilingsStaging.entities e ON e.entity_id = r.entity_id
+WHERE e.entity_ticker in (':tickers')
 ORDER BY r.reporting_period_end_date DESC;
