18a6203ba900f469e9517009fb7c8c41a15ef9d4
Javier <javier.garcia.d@gmail.com>
2024-05-12 12:46:45 -0500

    remove sql

--- edgar-filings-examples/sql/filings_by_ticker.sql
+++
@@ -1,9 +0,0 @@
--- @block : reports per entity_ticker
--- Purpose: This query returns the reports for a list of entities.
-SELECT DISTINCT r.source_report_identifier as accession_number,
-    r.properties ['document_type'] as document_type,
-    r.reporting_period_end_date as period_date
-FROM EdgarFilingsStaging.reports r
-    JOIN EdgarFilingsStaging.entities e ON e.entity_id = r.entity_id
-WHERE e.entity_ticker in (':tickers')
-ORDER BY r.reporting_period_end_date DESC;
